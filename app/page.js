ad33ea64b80a1461efaa2ff297803aba60b4aba4
Irfan Syahputra <irfansyahputrahadiyat@gmail.com>
2023-06-13 14:56:00 +0700

    second commit

--- app/page.js
+++ app/page.js
@@ -11,154 +11,187 @@ import udang from '../public/udang.png'
 import waktu from '../public/waktu.png'
 import pakan from '../public/pakan.png'
 import akses from '../public/akses.png'
-import Section from '../public/Section.png'
 import googleLogo from '../public/googleplay.png'
+import headerImage from '../public/Header-Image.jpg'
+import efisheryku from '../public/efisheryku-logo.png'
+import efarm from '../public/efarm-logo.png'
+import efresh from '../public/efresh-logo.png'
 
 export default function Home() {
   return (
     <main>
-      <Image style={{ width: "100%", height: "100%", position: "relative" }}
-        src={Section}
-      />
-      <span className='w-1/2 text-5xl font-black text-white' style={{ margin: 0, position: 'absolute', top: "40%", left: "10%" }}>
-        Tumbuh Bersama lebih dari<br />
-        <span style={{ color: "#FFEC94", fontWeight: "bold" }}>70.000 </span>Pembudidaya<br />
-        dari <span style={{ color: "#FFEC94", fontWeight: "bold" }}>280</span> Kota di Indonesia<br />
-        melalui Teknologi
-      </span>
-      <div style={{ backgroundColor: "black", height: "400px", position: "relative" }}>
-        <div className='flex space-x-10 w-4/5 mx-32' style={{ position: 'absolute', top: "10%", fontSize: "20px" }}>
-          <div className='w-1/3 flex'>
-            <Image
-              src={ikan}
-              width={200}
-              alt="ikan"
-            />
-            <span className='text-white ml-10'>
-              Memfasilitasi<br />
-              <span className='text-ijo'>1,105 Triliun</span><br />
-              Total Transaksi Penjualan Ikan Tawar
-            </span>
-          </div>
-          <div className='w-1/3 flex'>
-            <Image
-              src={udang}
-              width={200}
-              alt="udang"
-            />
-            <span className='text-white ml-10'>
-              Memfasilitasi<br />
-              <span className='text-ijo'>1,125 Triliun</span><br />
-              Total Transaksi Penjualan Udang
-            </span>
-          </div>
-          <div className='w-1/3 flex'>
-            <Image
-              src={waktu}
-              width={200}
-              alt="waktu"
-            />
-            <span className='text-white ml-10'>
-              Mengakselerasi Waktu Panen hingga<br />
-              <span className='text-ijo'>74 Hari</span><br />
-            </span>
+      {/* <Image style={{ width: "100%", height: "100%", position: "relative" }}
+        src={headerImage}
+      /> */}
+      <div style={{
+        backgroundImage: `url(${headerImage.src})`,
+        width: "100%",
+        height: "780px",
+        backgroundSize: "cover",
+        position: "relative",
+        backgroundPosition: "center center",
+        padding: "80px 0px 200px 0px"
+      }}>
+        <div style={{ display: "flex", minHeight: "500px", alignItems: "center", maxWidth: "1140px", marginLeft: "auto", marginRight: "auto" }}>
+          <span className='text-white header' style={{ fontSize: "41px", fontWeight: 800, lineHeight: "54.96px" }} >
+            Tumbuh Bersama lebih dari<br />
+            <span style={{ color: "#FFEC94" }}>70.000 </span>Pembudidaya<br />
+            dari <span style={{ color: "#FFEC94" }}>280</span> Kota di Indonesia<br />
+            melalui Teknologi
+          </span>
+        </div>
+        <div className='bg-gradient-to-b from-transparent to-black text' style={{ height: "167.2px", position: "relative" }}>
+          <div className='flex space-x-10 w-4/5 mx-32' style={{ position: 'absolute', fontSize: "20px" }}>
+            <div className='w-1/3 flex '>
+              <Image style={{ width: "91px", height: "91px" }}
+                src={ikan}
+                alt="ikan"
+              />
+              <span className='text-white ml-10 leading-[36px]'>
+                Memfasilitasi<br />
+                <span className='text-ijo font-[700] text-[24px]'>1,105 Triliun Rupiah</span><br />
+                Total Transaksi Penjualan Ikan Tawar
+              </span>
+            </div>
+            <div className='w-1/3 flex '>
+              <Image style={{ width: "91px", height: "91px" }}
+                src={udang}
+                width={200}
+                alt="udang"
+              />
+              <span className='text-white ml-10'>
+                Memfasilitasi<br />
+                <span className='text-ijo font-[700] text-[24px]'>1,125 Triliun Rupiah</span><br />
+                Total Transaksi Penjualan Udang
+              </span>
+            </div>
+            <div className='w-1/3 flex'>
+              <Image style={{ width: "78px", height: "78px" }}
+                src={waktu}
+                width={200}
+                alt="waktu"
+              />
+              <span className='text-white ml-10'>
+                Mengakselerasi Waktu Panen hingga<br />
+                <span className='text-ijo font-[700] text-[24px]'>74 Hari</span><br />
+              </span>
+            </div>
           </div>
         </div>
-        <div className='flex space-x-10 w-4/5 ml-80 mt-6' style={{ position: 'absolute', top: "50%", fontSize: "20px" }}>
-          <div className='w-1/3 flex'>
-            <Image
-              src={pakan}
-              width={120}
-              alt="pakan"
-            />
-            <span className='text-white ml-10'>
-              Memfasilitasi<br />
-              <span className='text-ijo'>1,125 Triliun</span><br />
-              Total Transaksi Penjualan Udang
-            </span>
-          </div>
-          <div className='w-1/3 flex'>
-            <Image
-              src={akses}
-              width={150}
-              alt="akses"
-            />
-            <span className='text-white ml-10'>
-              Memfasilitasi<br />
-              <span className='text-ijo'>1,125 Triliun</span><br />
-              Total Transaksi Penjualan Udang
-            </span>
+        <div className='bg-black' style={{ height: "199.2px" }}>
+          <div className='flex space-x-10 w-4/5 ml-48 justify-center items-center mt-6' style={{ position: 'absolute', top: "95%", fontSize: "20px" }}>
+            <div className='w-1/3 flex'>
+              <Image style={{ width: "86px", height: "94px" }}
+                src={pakan}
+                width={120}
+                alt="pakan"
+              />
+              <span className='text-white ml-10'>
+                Memfasilitasi<br />
+                <span className='text-ijo font-[700] text-[24px]'>1,99 Triliun Rupiah</span><br />
+                Total Transaksi Penjualan Pakan Ikan dan Udang
+              </span>
+            </div>
+            <div className='w-1/3 flex'>
+              <Image style={{ width: "94px", height: "94px" }}
+                src={akses}
+                width={150}
+                alt="akses"
+              />
+              <span className='text-white ml-10'>
+                Memfasilitasi<br />
+                <span className='text-ijo font-[700] text-[24px]'>15.000</span><br />
+                Pembudidaya ke Institusi Finansial, Terdaftar dan Diawasi OJK
+              </span>
+            </div>
           </div>
         </div>
       </div>
-      <div className='text-center'>
-        <div className='text-ijo mb-4 pt-4' style={{ fontWeight: 900, fontSize: "48px" }}>
+
+      <div className='text-center mt-40'>
+        <div className='text-ijo mb-4 pt-4 header' style={{ fontWeight: 900, fontSize: "48px" }}>
           Misi Kami
         </div>
-        <div className='text-3xl font-bold'>
+        <div className='header' style={{ fontWeight: 600, color: "#424242", fontSize: "28px", lineHeight: "36px", display: "inline" }}>
           Menjembatani Kesenjangan Digital, Memenuhi Kebutuhan Pangan Dunia
         </div>
       </div>
-      <div className='flex text-center px-60'>
-        <div className='w-1/3 text-2xl font-medium' style={{ height: "500px" }}>
-          <Image className='m-auto mt-12'
+
+      <div className='flex text-center gap-3' style={{ height: "530px", maxWidth: "1140px", marginRight: "auto", marginLeft: "auto" }}>
+        <div className='w-1/3' style={{ height: "509px", width: "380px", padding: "10px 10px 10px 10px", minHeight: "auto", minWidth: "auto" }}>
+          <Image className='m-auto mt-12' style={{ width: "324px", height: "335px", maxWidth: "100%" }}
             src={a1}
-            width={300}
             alt="a1"
           />
-          Memenuhi Kebutuhan Pangan Dunia melalui Akuakultur
+          <div className='header' style={{ height: "108px", width: "360px", margin: "0 0 16px 0", fontSize: "28px", display: "block", fontWeight: "600", color: "#424242", lineHeight: "36px" }}>
+            Memenuhi Kebutuhan Pangan Dunia melalui Akuakultur
+          </div>
         </div>
-        <div className='w-1/3 text-2xl font-medium' style={{ height: "500px" }}>
-          <Image className='m-auto mt-12'
+        <div className='w-1/3' style={{ height: "509px", width: "380px", padding: "10px 10px 10px 10px", minHeight: "auto", minWidth: "auto" }}>
+          <Image className='m-auto mt-12' style={{ width: "290px", height: "357px", maxWidth: "100%" }}
             src={a2}
-            width={250}
-            alt="a1"
+            alt="a2"
           />
-          Menyediakan Teknologi Terjangkau
+          <div className='header' style={{ height: "108px", width: "360px", margin: "0 0 16px 0", fontSize: "28px", display: "block", fontWeight: "600", color: "#424242", lineHeight: "36px" }}>
+            Menyediakan Teknologi Terjangkau
+          </div>
         </div>
-        <div className='w-1/3 text-2xl font-medium' style={{ height: "500px" }}>
-          <Image className='m-auto mt-12'
+        <div className='w-1/3' style={{ height: "509px", width: "380px", padding: "10px 10px 10px 10px", minHeight: "auto", minWidth: "auto" }}>
+          <Image className='m-auto mt-12' style={{ width: "360px", height: "371.712px", maxWidth: "100%" }}
             src={a3}
             width={300}
-            alt="a1"
+            alt="a3"
           />
-          Membangun Ekonomi Digital yang Inklusif
+          <div className='header' style={{ height: "108px", width: "360px", margin: "0 0 16px 0", fontSize: "28px", display: "block", fontWeight: "600", color: "#424242", lineHeight: "36px" }}>
+            Membangun Ekonomi Digital yang Inklusif
+          </div>
         </div>
       </div>
-      <div className='px-56 text-center font-thin mb-6'>
-        <div>
-          Per tahun 2050, kebutuhan untuk protein hewani akan meningkat hingga 52%. Sayangnya, sekitar 660 juta orang masih akan menghadapi isu kelaparan di tahun 2030. Akuakultur, sebagai sektor bisnis dengan pertumbuhan yang pesat dan penyedia sumber protein hewani dengan konversi pakan paling efisien, memiliki potensi untuk berkontribusi secara signifikan terhadap ketahanan pangan.
-          <br />
-          eFishery percaya bahwa “The Future is Aquaculture”. Menggunakan kemajuan teknologi, eFishery membangun value chain yang sistematis untuk bisnis budidaya ikan dan udang, menghasilkan ekosistem yang terintegrasi dan berkelanjutan untuk memperkuat ketahanan pangan global di masa mendatang.
+
+      <div className='w-full relative'>
+        <div style={{ width: "1140px", marginLeft: "auto", marginRight: "auto" }} >
+          <p className='text-center mb-6' style={{ color: "#424242", lineHeight: "36px", fontWeight: 100 }}>
+            Per tahun 2050, kebutuhan untuk protein hewani akan meningkat hingga 52%. Sayangnya, sekitar 660 juta orang masih akan menghadapi isu kelaparan di tahun 2030. Akuakultur, sebagai sektor bisnis dengan pertumbuhan yang pesat dan penyedia sumber protein hewani dengan konversi pakan paling efisien, memiliki potensi untuk berkontribusi secara signifikan terhadap ketahanan pangan.
+          </p>
+          <p className='text-center mb-6' style={{ color: "#424242", lineHeight: "36px", fontWeight: 100 }}>
+            eFishery percaya bahwa “The Future is Aquaculture”. Menggunakan kemajuan teknologi, eFishery membangun value chain yang sistematis untuk bisnis budidaya ikan dan udang, menghasilkan ekosistem yang terintegrasi dan berkelanjutan untuk memperkuat ketahanan pangan global di masa mendatang.
+          </p>
         </div>
       </div>
-      <div className='flex flex-col items-center justify-center mb-4'>
-        <button className='bg-ijo rounded-full px-8 py-4 text-xl text-white flex flex-col items-center justify-center'>Baca Lebih Lanjut</button>
+      <div style={{ height: "108px" }}>
+        <div className='flex items-center justify-center mb-4' >
+          <button className='bg-ijo rounded-full px-8 py-4 text-xl text-white flex flex-col items-center justify-center' style={{ height: "70px", width: "265.125px" }} >Baca Lebih Lanjut</button>
+        </div>
       </div>
-      <div className='pt-4 bg-gray-200' style={{ height: "800px" }}>
-        <div className='text-4xl text-center mb-24 mt-6'>
+
+      <div className='pt-4' style={{ height: "800px", backgroundColor: "#EDEDEF" }}>
+        <div className='text-center mb-24 sora' style={{ fontSize: "48px" }}>
           Solusi Kami Untuk Dunia Aquaculture
         </div>
         <div className='flex mb-3 mt-4 text-center px-60'>
-          <div className='w-1/3 bg-white pt-4 text-left px-4 mx-4' style={{ height: "600px", borderRadius: "60px", position: "relative" }}>
+          <div className='w-1/3 bg-white pt-4 text-left px-4 mx-5' style={{ height: "600px", borderRadius: "60px", position: "relative" }}>
             <Image className='m-auto' style={{ marginTop: "-100px" }}
               src={b1}
-              width={300}
-              height={200}
+              width={400}
               alt="b1"
             />
-            <div className='text-4xl text-ijo mb-2 font-bold'>
-              eFisheryku
+            <div className='mb-2 mt-6'>
+              <Image style={{ height: "44px" }}
+                src={efisheryku}
+                alt="efisheryku"
+              />
             </div>
-            <div className='text-2xl mb-4'>
-              Solusi untuk<br />
+            <span className='text-[21px] sora'>
+              <span className='font-thin'>
+                Solusi untuk<br />
+              </span>
               Pembudidaya Ikan
-            </div>
-            <div className='text-md'>
+            </span>
+            <div className='text-md mt-2 text-[#424242]'>
               Layanan lengkap, dari hulu ke hilir untuk Pembudidaya ikan. Rasakan kemudahan transaksi pakan, akses ke institusi finansial yang terdaftar dan diawasi OJK, platform penjualan hasil panen ikan yang menguntungkan. Semua tersedia untuk memajukan bisnis Pembudidaya!
             </div>
             <div className='flex justify-between mb-6 ' style={{ position: "absolute", bottom: 0, width: "90%" }} >
-              <button className='bg-ijo rounded-full px-2 text-xs text-white flex flex-col items-center justify-center'>Cari Tahu Lebih Lanjut</button>
+              <button className='bg-ijo rounded-full px-2 text-[13px] text-white flex flex-col items-center justify-center leading-[13px]'>Cari Tahu Lebih Lanjut</button>
               <Link href="#">
                 <Image
                   src={googleLogo}
@@ -168,25 +201,30 @@ export default function Home() {
               </Link>
             </div>
           </div>
-          <div className='w-1/3 bg-white pt-4 text-left px-4 mr-4' style={{ height: "600px", borderRadius: "60px", position: "relative" }}>
+          <div className='w-1/3 bg-white pt-4 text-left px-4 mx-5' style={{ height: "600px", borderRadius: "60px", position: "relative" }}>
             <Image className='m-auto' style={{ marginTop: "-100px" }}
               src={b2}
               width={260}
               height={200}
               alt="b2"
             />
-            <div className='text-4xl text-ijo mb-2 font-bold'>
-              eFarm
+            <div className='mb-2 mt-2'>
+              <Image style={{ height: "35px" }}
+                src={efarm}
+                alt="efarm"
+              />
             </div>
-            <div className='text-2xl mb-4'>
-              Solusi untuk<br />
+            <span className='text-[21px] sora'>
+              <span className='font-thin'>
+                Solusi untuk<br />
+              </span>
               Pembudidaya Udang
-            </div>
-            <div className='text-md'>
+            </span>
+            <div className='text-md mt-2 text-[#424242]'>
               Ekosistem budidaya udang terpadu meliputi pencegahan penyakit dan wabah, manajemen kualitas air tambak, dan perekomendasian produk-produk SaProTam terbaik untuk Pembudidaya Udang. Solusi ini meningkatkan produktivitas dan keberhasilan budidaya udang secara efektif melalui teknologi.
             </div>
             <div className='flex justify-between mb-6 ' style={{ position: "absolute", bottom: 0, width: "90%" }} >
-              <button className='bg-ijo rounded-full px-2 text-xs text-white flex flex-col items-center justify-center'>Cari Tahu Lebih Lanjut</button>
+              <button className='bg-ijo rounded-full px-2 text-[13px] text-white flex flex-col items-center justify-center leading-[13px]'>Cari Tahu Lebih Lanjut</button>
               <Link href="#">
                 <Image
                   src={googleLogo}
@@ -196,25 +234,32 @@ export default function Home() {
               </Link>
             </div>
           </div>
-          <div className='w-1/3 bg-white pt-4 text-left px-4' style={{ height: "600px", borderRadius: "60px", position: "relative" }}>
-            <Image className='m-auto' style={{ marginTop: "-100px" }}
+          <div className='w-1/3 bg-white pt-4 text-left px-4 mx-5' style={{ height: "600px", borderRadius: "60px", position: "relative" }}>
+            <Image className='m-auto' style={{ marginTop: "-110px" }}
               src={b3}
-              width={260}
+              width={250}
               height={200}
               alt="b3"
             />
-            <div className='text-4xl text-ijo mb-2 font-bold'>
-              eFresh
+            <div className='mb-2 mt-2'>
+              <Image style={{ height: "35px" }}
+                src={efresh}
+                alt="efresh"
+              />
             </div>
-            <div className='text-2xl mb-4'>
-              Solusi untuk<br />
+            <span className='text-[21px] sora'>
+              <span className='font-thin'>
+                Solusi untuk<br />
+              </span>
               Pembeli & Konsumen
-            </div>
-            <div className='text-md'>
+            </span>
+            <div className='text-md mt-2 text-[#424242]'>
               Memastikan ikan berkualitas terbaik dapat dinikmati di seluruh penjuru negeri dengan mendistribusikan hasil panen Pembudidaya dalam bentuk hidup, segar, dan beku.
             </div>
-            <div className='flex justify-between mb-6 ' style={{ position: "absolute", bottom: 0, width: "90%" }} >
-              <button className='bg-ijo rounded-full px-2 text-xs text-white flex flex-col items-center justify-center'>Cari Tahu Lebih Lanjut</button>
+            <div className='flex justify-between mb-6' style={{ position: "absolute", bottom: 0, width: "90%" }} >
+              <button className='bg-ijo rounded-full px-2 text-[13px] text-white flex flex-col items-center justify-center leading-[13px]'>
+                Cari Tahu Lebih Lanjut
+              </button>
               <Link href="#">
                 <Image
                   src={googleLogo}
--- components/nav.js
+++ components/nav.js
@@ -1,22 +1,28 @@
+'use client'
 import Image from 'next/image'
 import logo from '../public/logo.png'
 import Link from 'next/link'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from 'react';
 
 export default function Nav() {
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+    };
     return (
-        <div className="flex text-center items-center justify-center sticky top-0 z-50 bg-white" style={{ height: "80px" }}>
-            <div className="w-1/4 text-left">
+        <div className="flex text-center items-center  lg:justify-center sticky top-0 z-50 bg-white mx-auto border-2" style={{ minHeight: "80px", fontSize: "16px" }}>
+            <div className="w-1/5">
                 <Link href="/">
-                    <Image
+                    <Image style={{ width: "148.194px", height: "37.0417" }}
                         src={logo}
-                        width={150}
                         alt="Logo"
                     />
                 </Link>
             </div>
-            <div className="w-2/4">
-                <ul className="flex space-x-10">
-                    <li>
+            <div className="w-2/4 ">
+                <ul className="flex space-x-10 max-lg:invisible" style={{ width: "912px" }}>
+                    <li >
                         <Link className="text-ijo hover:text-slate-950" href="#">Home</Link>
                     </li>
                     <li>
@@ -36,6 +42,48 @@ export default function Nav() {
                     </li>
                 </ul>
             </div>
-        </div>
+
+            {/* @Media Mobile */}
+
+            {/* <div onClick={handleNav} className='lg:hidden z-10' style={{ position: "fixed", right: 0, padding: 10 }}> */}
+            <div onClick={handleNav} className='lg:hidden z-10' style={{ marginLeft: "auto" }}>
+                {nav ? (
+                    <AiOutlineClose className='text-ijo' size={30} />
+                ) : (
+                    <AiOutlineMenu className='text-ijo' size={30} />
+                )}
+            </div>
+
+            <div>
+                <div
+                    className={
+                        nav
+                            ? 'lg:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300'
+                            : 'lg:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300'
+                    }
+                >
+                    <ul>
+                        <li onClick={handleNav} className='p-4 text-ijo'>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-ijo'>
+                            <Link href='#'>Tentang Kami</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-ijo'>
+                            <Link href='#'>Produk</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-ijo'>
+                            <Link href='#'>Karir</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-ijo'>
+                            <Link href='#'>Blog</Link>
+                        </li>
+                        <li onClick={handleNav} className='p-4 text-ijo'>
+                            <Link href='#'>Hubungi Kami</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div >
     )
 }
